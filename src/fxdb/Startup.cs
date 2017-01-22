8448563b2872420ff2359d40654cc57c664e5cbc
Mitchell Monahan <soccermitchy@gmail.com>
2017-01-22 09:39:44 -0500

    Set file upload limit

--- src/fxdb/Startup.cs
+++ src/fxdb/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using fxdb.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -45,6 +46,11 @@ namespace fxdb
 
             // DI for fxdb - Repositories
             services.AddSingleton<IEffectRepository, MockEffectRepository>();
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = int.MaxValue;
+                x.MultipartBodyLengthLimit = int.MaxValue;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
